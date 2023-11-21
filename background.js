50da608050657eba44a3e66a9ea4812dd6380f8e
Esteban Samayoa Castillo <61565155+estebansamayoa1@users.noreply.github.com>
2023-11-20 21:34:42 -0600

    Cambios Daltonismo

--- background.js
+++
@@ -1,6 +0,0 @@
-
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.type === "selectedText") {
-        console.log("Selected text: ", message.text);
-    }
-});
--- popup.js
+++ popup.js
@@ -25,4 +25,10 @@ document.getElementById('option4').addEventListener('click', () => {
     });
   });
 
+document.getElementById('applyFilters').addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {action: "option4"});
+    });
+  });
+
 
