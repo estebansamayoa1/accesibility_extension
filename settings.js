855c4a5a046ab8a95b7e574a72c43e9aad9b2500
Nickolas Nolte <nickolasnolte@ufm.edu>
2023-11-23 00:16:13 -0600

    Tutorial update

--- settings.js
+++ settings.js
@@ -2,3 +2,7 @@ document.getElementById('settingsButton').addEventListener('click', function() {
     window.open('settings.html', '_blank');
   });
   
+  document.getElementById('tutorialbutton').addEventListener('click', function() {
+    window.open('tutorial.html', '_blank');
+  });
+  
